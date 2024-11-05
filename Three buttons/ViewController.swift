94154f1c60c3d80212d36488fff85490c7b4aa8c
Zuleikha Usmanova <>
2024-11-05 14:16:53 +0300

    Three buttons

---
+++ Three buttons/ViewController.swift
@@ -0,0 +1,173 @@
+//
+//  ViewController.swift
+//  Three buttons
+//
+//  Created by aeroclub on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var firstButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        
+        button.setTitle("First Button", for: .normal)
+        button.backgroundColor = .systemGray3
+        button.setImage(UIImage(systemName: "arrow.forward.circle.fill"), for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(firstButtonTap), for: [.touchDown, .touchUpInside, .touchUpOutside])
+        button.configuration?.titlePadding = 8
+        button.configuration?.imagePadding = 8
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 14, bottom: 10, trailing: 14)
+        
+        return button
+    }()
+    
+    private lazy var secondButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        
+        button.setTitle("Second Medium Button", for: .normal)
+        button.backgroundColor = .systemGray3
+        button.setImage(UIImage(systemName: "arrow.forward.circle.fill"), for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(secondButtonTap), for: [.touchUpInside, .touchDown, .touchUpOutside])
+        button.configuration?.titlePadding = 8
+        button.configuration?.imagePadding = 8
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 14, bottom: 10, trailing: 14)
+        
+        return button
+    }()
+    
+    private lazy var thirdButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        
+        button.setTitle("Third", for: .normal)
+        button.backgroundColor = .systemGray3
+        button.setImage(UIImage(systemName: "arrow.forward.circle.fill"), for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(thirdButtonTap), for: .touchUpInside)
+        button.configuration?.titlePadding = 8
+        button.configuration?.imagePadding = 8
+        button.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 14, bottom: 10, trailing: 14)
+        
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.addArrangedSubview(firstButton)
+        stackView.addArrangedSubview(secondButton)
+        stackView.addArrangedSubview(thirdButton)
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        
+        return stackView
+    }()
+
+    private var isPressed = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    private func setup() {
+        view.addSubview(firstButton)
+        view.addSubview(secondButton)
+        view.addSubview(thirdButton)
+        
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        secondButton.translatesAutoresizingMaskIntoConstraints = false
+        thirdButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            
+            secondButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondButton.topAnchor.constraint(equalTo: firstButton.bottomAnchor, constant: 10),
+            
+            thirdButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            thirdButton.topAnchor.constraint(equalTo: secondButton.bottomAnchor, constant: 10)
+            
+        ])
+    }
+    
+    @objc func firstButtonTap() {
+      
+        if isPressed {
+            isPressed = false
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.firstButton.transform = .identity
+            }
+        } else {
+            isPressed = true
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.firstButton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            }
+        }
+    }
+    
+    @objc func secondButtonTap() {
+        
+        if isPressed {
+            isPressed = false
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.secondButton.transform = .identity
+            }
+        } else {
+            isPressed = true
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.secondButton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            }
+        }
+    }
+    
+    @objc func thirdButtonTap() {
+        if isPressed {
+            isPressed = false
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.thirdButton.transform = .identity
+            }
+        } else {
+            isPressed = true
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .curveEaseInOut) {
+                self.thirdButton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            }
+        }
+        present(ButtonTap(), animated: true)
+    }
+}
+
